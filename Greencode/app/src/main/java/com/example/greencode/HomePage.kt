053f3695e0216fd45cdad50b9353ef2e0f60d8c3
Lucas Gabriel <lucas.castro7@hotmail.com>
2020-04-11 14:27:57 -0300

    Gerando QrCode para teste

--- Greencode/app/src/main/java/com/example/greencode/HomePage.kt
+++ Greencode/app/src/main/java/com/example/greencode/HomePage.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_home_page.*
+import net.glxn.qrgen.android.QRCode
 
 class HomePage : AppCompatActivity() {
 
@@ -14,10 +16,17 @@ class HomePage : AppCompatActivity() {
     }
 
     fun showQrCodeScreen(V : View){
+        createQrCode("{ email: teste@teste, senha:teste}")
         qr_code_screen.visibility = View.VISIBLE
     }
 
     fun hideQrCodeScreen(V : View){
         qr_code_screen.visibility = View.INVISIBLE
+    }
+
+    fun createQrCode( usuario:String ){
+        val gson = Gson()
+        val bitmap = QRCode.from(gson.toJson(usuario)).withSize(1000, 1000).bitmap()
+        qr_code.setImageBitmap(bitmap)
     }
 }
