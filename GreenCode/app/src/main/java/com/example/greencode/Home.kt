27dbc6892fb34515cbcbdddcb69a27d0f62cbd55
kd_o_guh <gugafsdb@gmail.com>
2020-06-14 13:47:53 -0300

    home e marketplace

--- GreenCode/app/src/main/java/com/example/greencode/Home.kt
+++ GreenCode/app/src/main/java/com/example/greencode/Home.kt
@@ -1,12 +1,19 @@
 package com.example.greencode
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun goToMarketplace(v:View){
+        val intent = Intent(this, Marketplace::class.java)
+        startActivity(intent)
     }
 }
---
+++ GreenCode/app/src/main/java/com/example/greencode/Marketplace.kt
@@ -0,0 +1,11 @@
+package com.example.greencode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Marketplace : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marketplace)
+    }
+}
