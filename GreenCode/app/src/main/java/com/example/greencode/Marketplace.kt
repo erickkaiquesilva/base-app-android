95385dcb5da416d73033e632e9768f2ef057f6ae
kd_o_guh <gugafsdb@gmail.com>
2020-06-16 17:13:18 -0300

    marketplace app

--- GreenCode/app/src/main/java/com/example/greencode/Marketplace.kt
+++ GreenCode/app/src/main/java/com/example/greencode/Marketplace.kt
@@ -7,5 +7,19 @@ class Marketplace : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_marketplace)
+        createFragment()
+    }
+
+    fun createFragment() {
+        val transaction_highlight = supportFragmentManager.beginTransaction()
+        val transaction_most_searched = supportFragmentManager.beginTransaction()
+        var i = 0
+        while (i < 5) {
+            transaction_highlight.add(R.id.highlights_list, Highlight())
+            transaction_most_searched.add(R.id.most_searched_list, MostSearched())
+            i++
+        }
+        transaction_highlight.commit()
+        transaction_most_searched.commit()
     }
 }
