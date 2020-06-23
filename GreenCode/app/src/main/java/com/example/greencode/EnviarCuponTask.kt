2bcc73b848619dec8eaf48e12f2a9b64eaed3beb
Lucas Gabriel <lucas.castro7@hotmail.com>
2020-06-23 16:26:06 -0300

    quase pronto

--- GreenCode/app/src/main/java/com/example/greencode/EnviarCuponTask.kt
+++ GreenCode/app/src/main/java/com/example/greencode/EnviarCuponTask.kt
@@ -3,14 +3,13 @@ package com.example.greencode
 import android.os.AsyncTask
 import feign.FeignException
 
-class EnviarCuponTask : AsyncTask<String, Void, String>() {
+class EnviarCuponTask : AsyncTask<Usuario, Void, String>() {
 
-    override fun doInBackground(vararg params: String?):String? {
-
+    override fun doInBackground(vararg params: Usuario?):String? {
         try {
             return EmailApi.criar().enviarNovoCupon(params[0]!!)
         }catch (e: FeignException){
-            return null
+            return e.toString()
         }
 
     }
